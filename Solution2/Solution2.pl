836460f67076d7dcc0ab8dce8c89d03c7bece54c
oriel2323 <oriel2510@gmail.com>
2026-06-11 00:23:03 +0300

    finished my_prefix and my_member

--- Solution2/Solution2.pl
+++ Solution2/Solution2.pl
@@ -11,5 +11,21 @@ my_reverse([H|T], R) :-
     my_reverse(T,Treversed),
     append(Treversed,[H],R).
     
+
 % 2) 
 
+% empty list is a prefix of every list
+my_prefix([],_).
+
+% heads must be equal, then check tails
+my_prefix([H|T1],[H|T2]) :-
+    my_prefix(T1,T2). 
+
+% 3) 
+
+% X is the head of the list
+my_member(X,[X|_]).
+
+% otherwise, search X in the tail
+my_member(X,[_|T]) :-
+    my_member(X,T). 
