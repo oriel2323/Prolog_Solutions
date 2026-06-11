e90e01d0d2ed9290b975211abc3ff6caacfc989f
i-sh-sh <shshteinberg@gmail.com>
2026-06-11 17:40:14 +0300

    Update Solution2.pl

--- Solution2/Solution2.pl
+++ Solution2/Solution2.pl
@@ -28,4 +28,60 @@ my_member(X,[X|_]).
 
 % otherwise, search X in the tail
 my_member(X,[_|T]) :-
-    my_member(X,T). 
+    my_member(X,T).
+    
+% 4)
+
+% X appears at least twice if X is the head
+% and X also appears somewhere in the tail.
+my_member2(X, [X|T]) :-
+    my_member(X, T).
+
+% otherwise, continue searching in the tail.
+my_member2(X, [_|T]) :-
+    my_member2(X, T).
+
+
+% 5)
+
+% empty list is a palindrome.
+my_palindrom([]).
+
+% list with one element is a palindrome.
+my_palindrom([_]).
+
+% A list is a palindrome if the first and last elements are equal,
+% and the middle part is also a palindrome.
+my_palindrom([H|T]) :-
+    append(Middle, [H], T),
+    my_palindrom(Middle).
+
+
+% 6)
+
+% empty list is sorted.
+my_sorted([]).
+
+% list with one element is sorted.
+my_sorted([_]).
+
+% A list is sorted if the first element is smaller or equal to the second,
+% and the tail is also sorted.
+my_sorted([X, Y | T]) :-
+    X =< Y,
+    my_sorted([Y|T]).
+
+
+% 7)
+
+% If the list is empty, insert X as the only element.
+my_insert(X, [], [X]).
+
+% If X should come before the head, place it there.
+my_insert(X, [H|T], [X,H|T]) :-
+    X =< H.
+
+% Otherwise, keep the head and insert X into the tail.
+my_insert(X, [H|T], [H|Z]) :-
+    X > H,
+    my_insert(X, T, Z).
